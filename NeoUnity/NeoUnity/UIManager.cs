1c92e8626b4d7f32b3aed380d92abd0cd9ac5bcf
no23 <sethrobbins2003@yahoo.com>
2017-03-15 21:04:55 -0700

    Added things

--- NeoUnity/NeoUnity/UIManager.cs
+++ NeoUnity/NeoUnity/UIManager.cs
@@ -1,43 +1,104 @@
-﻿using UnityEngine;
+﻿using NeoUnity;
+using UnityEngine;
+using UnityEngine.UI;
 
 public class UIManager : MonoBehaviour
 {
+    public GameObject LeftMenuPanel;
+    public GameObject SelectedObject;
 
-	public GameObject SelectedObject;
+    public Text QueryText;
+
+    private const float _tSpeed = 100;
+    private bool _leftMenu;
+    private bool _leftMenuMoving;
+    
+    
 
 
     void Update()
     {
-		if (Input.GetMouseButton (0)) {
-			Ray r = Camera.main.ScreenPointToRay (Input.mousePosition);
-			RaycastHit rh;
-			if (Physics.Raycast (r, out rh, float.PositiveInfinity)) {
-				if (rh.collider.tag == "Node") {
-					SelectedObject = rh.collider.gameObject;
-				}
-			}
-		}
+        if (_leftMenuMoving)
+        {
+            RectTransform rt = LeftMenuPanel.GetComponent<RectTransform>();
+            if (_leftMenu)
+            {
+                if (rt.position.x < -220)
+                {
+                    _leftMenuMoving = false;
+                    _leftMenu = false;
+                    rt.position = new Vector3(-220, Screen.height, 0);
+                }
+                else
+                {
+                    rt.position -= new Vector3(_tSpeed, 0, 0) * Time.deltaTime;
+                }
+            }
+            else
+            {
+                if (rt.position.x >= 0)
+                {
+                    _leftMenuMoving = false;
+                    _leftMenu = true;
+                    rt.position = new Vector3(0, Screen.height, 0);
+                }
+                else
+                {
+                    rt.position += new Vector3(_tSpeed, 0, 0) * Time.deltaTime;
+                }
 
-		if (SelectedObject) {
-			SelectedObject.transform.position = Camera.main.ScreenToWorldPoint (Input.mousePosition+ new Vector3(0,0,10));
-		}
+            }
+        }
 
-		if (Input.GetMouseButtonUp (0)) {
-			SelectedObject = null;
-		}
-		if (Input.GetMouseButton (1)) {
-			float x = -Input.GetAxis ("Mouse X");
-			float y = -Input.GetAxis ("Mouse Y");
-			Debug.Log ($"x: {x} y: {y}");
-			this.transform.position += new Vector3 (x, y);
-		}
+        if (Input.GetMouseButton(0))
+        {
+            Ray r = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit rh;
+            if (Physics.Raycast(r, out rh, float.PositiveInfinity))
+            {
+                if (rh.collider.tag == "Node")
+                {
+                    SelectedObject = rh.collider.gameObject;
+                }
+            }
+        }
+
+        if (SelectedObject)
+        {
+            SelectedObject.transform.position = Camera.main.ScreenToWorldPoint(Input.mousePosition + new Vector3(0, 0, 10));
+        }
+
+        if (Input.GetMouseButtonUp(0))
+        {
+            SelectedObject = null;
+        }
+        if (Input.GetMouseButton(1))
+        {
+            float x = -Input.GetAxis("Mouse X");
+            float y = -Input.GetAxis("Mouse Y");
+            Debug.Log($"x: {x} y: {y}");
+            this.transform.position += new Vector3(x, y);
+        }
 
 
     }
 
-	void OnDrawGizmos(){
-		Vector3 p = Camera.main.ScreenToWorldPoint(Input.mousePosition + new Vector3(0,0,10));
-		Gizmos.color = Color.yellow;
-		Gizmos.DrawSphere(p,1);
-	}
+    void OnDrawGizmos()
+    {
+        Vector3 p = Camera.main.ScreenToWorldPoint(Input.mousePosition + new Vector3(0, 0, 10));
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawSphere(p, 1);
+    }
+    public void RunQuery()
+    {
+        NeoUnity.Neo4j.Server.Query(QueryText.text);
+    }
+    public void ToggleMenu()
+    {
+        if (!_leftMenuMoving)
+        {
+            _leftMenuMoving = true;
+        }
+    }
+
 }
